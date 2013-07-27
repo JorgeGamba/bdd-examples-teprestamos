10cc62e734067f602a9b173d39163b89fb6879bb
Jorge Gamba <jorgegamba@gmail.com>
2013-07-27 15:44:04 -0500

    Added not implemented steps for Loan interest rate calculate feature

---
+++ src/spec/BDD.Examples.TePrestamos.Specs/Steps/CalculoDeTasaDeInteresSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BDD.Examples.TePrestamos.Specs.Steps
+{
+    [Binding]
+    public class CalculoDeTasaDeInteresSteps
+    {
+        [Given]
+        public void Dado_que_el_cliente_es_desplazado_por_la_violencia()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given]
+        public void Dado_que_el_cliente_es_madre_cabeza_de_hogar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given]
+        public void Dado_que_el_cliente_no_es_desplazado_ni_madre_cabeza_de_hogar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When]
+        public void Cuando_se_calcule_su_interés()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void Entonces_deberían_descontársele_P0_puntos(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void Entonces_el_total_no_debería_exceder_P0(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void Entonces_no_deberían_realizarse_descuentos_a_su_tasa_de_interés()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
